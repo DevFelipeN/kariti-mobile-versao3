ba07b73900e7f83e7cdd0193d0993c17a74e9174
Felipe <clickfelipeweb@gmail.com>
2024-01-29 15:10:49 -0300

    Criação de Banco de Dados

--- app/src/main/java/com/example/kariti/LoginActivity.java
+++ app/src/main/java/com/example/kariti/LoginActivity.java
@@ -2,13 +2,20 @@ package com.example.kariti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
     Button entrar, esqueciSenha;
+    BancoDados bancoDados;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +23,27 @@ public class LoginActivity extends AppCompatActivity {
 
         entrar = findViewById(R.id.buttonEntrarL);
         esqueciSenha = findViewById(R.id.buttonEsqueciSenhaL);
+        username = findViewById(R.id.editTextLogin);
+        password = findViewById(R.id.editTextSenha);
+        bancoDados = new BancoDados(this);
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mudarTelaInicio();
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(LoginActivity.this, "Por favor preencher todos os campos", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = bancoDados.checkuser(user); //Verificando no banco de Dados se Nome e senhas existem
+                    if(checkuserpass==true){
+                        Toast.makeText(LoginActivity.this, "Login Realizado com Sucesso! ", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(new Intent(getApplicationContext(), InicioActivity.class));
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Usuário e senha Inválidos! ", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
         esqueciSenha.setOnClickListener(new View.OnClickListener() {

14fee3472bb593cadca4d3466f96106fc06690ec
Dâmaris Campos <camposdamaris08@gmail.com>
2024-02-13 16:35:58 -0400

    Atualiza arquivo de cadastro de aluno

--- app/src/main/java/com/example/kariti/CadAlunoActivity.java
+++ app/src/main/java/com/example/kariti/CadAlunoActivity.java
@@ -2,17 +2,21 @@ package com.example.kariti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CadAlunoActivity extends AppCompatActivity {
     ImageButton voltar;
+    Button btnCadAluno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,27 +29,20 @@ public class CadAlunoActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-        Spinner spinnerTurma = findViewById(R.id.spinner_turma);
 
         btnCadAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mudarParaTelaFormCadAluno();
-            }
-        });
-        btnVisualizarAluno.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mudarParaTelaVisulaizarAluno();
+                mudarParaTelaVisulAluno();
             }
         });
 
     }
     public void mudarParaTelaFormCadAluno(){
-        Intent intent = new Intent(this, FormCadAlunoActivity.class);
+        Intent intent = new Intent(this, CadAlunoActivity.class);
         startActivity(intent);
     }
-    public void mudarParaTelaVisulaizarAluno(){
+    public void mudarParaTelaVisulAluno(){
         Intent intent = new Intent(this, VisualAlunoActivity.class);
         startActivity(intent);
         List<String> turmas = new ArrayList<>();
@@ -54,6 +51,7 @@ public class CadAlunoActivity extends AppCompatActivity {
         turmas.add("Turma 2");
         turmas.add("Turma 3");
 
+        Spinner spinnerTurma = findViewById(R.id.spinner_turma);
         // Criação do ArrayAdapter e configuração do Spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, turmas);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);

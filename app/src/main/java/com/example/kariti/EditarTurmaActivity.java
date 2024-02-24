a7a119b46f8e637d6f75161bcafd3d30e8b805a2
SayuryKato <skdp.ecp19@uea.edu.br>
2024-02-23 20:02:34 -0400

    feat: Tela Dados da Turma

---
+++ app/src/main/java/com/example/kariti/EditarTurmaActivity.java
@@ -0,0 +1,42 @@
+package com.example.kariti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EditarTurmaActivity extends AppCompatActivity {
+    ImageButton voltar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_turma);
+
+        ListView listView = findViewById(R.id.listView);
+        voltar = findViewById(R.id.imgBtnVoltar);
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        ArrayList<String> teste = new ArrayList<>();
+        teste.add("Antonio");
+        teste.add("Carlos");
+        teste.add("Gil");
+        teste.add("Guilherme");
+        teste.add("Miguel");
+        teste.add("Paulo");
+        teste.add("Toji");
+        teste.add("Via");
+        ArrayAdapter<String> adapterAlunos = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, teste);
+        listView.setAdapter(adapterAlunos);
+    }
+}

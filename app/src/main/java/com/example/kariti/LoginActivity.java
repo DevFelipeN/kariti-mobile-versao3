a963512288b314e30d0d02fe8c2343a13fdb761b
SayuryKato <skdp.ecp19@uea.edu.br>
2024-03-12 00:49:29 -0400

    Captura de foto

--- app/src/main/java/com/example/kariti/LoginActivity.java
+++ app/src/main/java/com/example/kariti/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/example/kariti/MainActivity.java
+++ app/src/main/java/com/example/kariti/MainActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;

403e1a38ca6f40c26bba7cec4f6b3055ccc61050
SayuryKato <skdp.ecp19@uea.edu.br>
2024-03-16 14:37:00 -0400

    Atualização campo gabrito

--- app/src/main/java/com/example/kariti/GabaritoActivity.java
+++ app/src/main/java/com/example/kariti/GabaritoActivity.java
@@ -86,7 +86,7 @@ public class GabaritoActivity extends AppCompatActivity {
             TextView textViewAlternativa = new TextView(this);
             textViewAlternativa.setText(String.valueOf(letra));
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            params.setMargins(42, 0, 42, 0);
+            params.setMargins(40, 0, 25, 0);
 
             textViewAlternativa.setLayoutParams(params); // Aplica os parâmetros de layout ao TextView
             textViewAlternativa.setGravity(Gravity.CENTER); // Centraliza o texto
@@ -113,7 +113,7 @@ public class GabaritoActivity extends AppCompatActivity {
             // Loop para criar Radio para as respostas
             for (int j = 0; j < quantidadeAlternativas; j++) {
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                params.setMargins(0, 25, 35, 0);
+                params.setMargins(0, 20, 20, 0);
 
                 RadioButton radioAlternativa = new RadioButton(this);
                 radioAlternativa.setLayoutParams(params);

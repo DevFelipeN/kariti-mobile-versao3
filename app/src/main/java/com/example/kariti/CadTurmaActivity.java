57a7ea6a199325ab8e4702fed06e0eb395f3cb14
dev_felipe <felipemartinsdonascimento4@gmail.com>
2024-03-18 22:34:12 -0400

    Implementação alunos anônimos

--- app/src/main/java/com/example/kariti/CadTurmaActivity.java
+++ app/src/main/java/com/example/kariti/CadTurmaActivity.java
@@ -10,15 +10,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class CadTurmaActivity extends AppCompatActivity {
     private ImageButton voltar;
     private Toolbar toolbar;
-    private EditText pesquisarAlunos, nomeTurma;
+    private EditText pesquisarAlunos, nomeTurma, alunosAnonimos;
+    ImageView menosAnonimos, maisAnonimos;
     private Button buttonIncluirAlunos, cadastrar;
     BancoDados bancoDados;
     Spinner spinnerBuscAluno;
@@ -36,10 +40,12 @@ public class CadTurmaActivity extends AppCompatActivity {
         nomeTurma = findViewById(R.id.editTextTurma);
         cadastrar = findViewById(R.id.buttonCadastrarTurma);
         spinnerBuscAluno = findViewById(R.id.spinnerBuscAluno);
+        alunosAnonimos = findViewById(R.id.editTextAlunosAnonimos);
+        menosAnonimos = findViewById(R.id.imageViewMenosAnonimos);
+        maisAnonimos = findViewById(R.id.imageViewMaisAnonimos);
         bancoDados = new BancoDados(this);
 
         ArrayList<String> nomesAluno = (ArrayList<String>) bancoDados.obterNomesAlunos();
-        ArrayList<String> aluno = (ArrayList<String>) bancoDados.obterNomesAlunos();
         SpinnerAdapter adapter = new SpinnerAdapter(this, nomesAluno);
         spinnerBuscAluno.setAdapter(adapter);
         AdapterExclAluno al = new AdapterExclAluno(this, nomesAluno);
@@ -48,12 +54,32 @@ public class CadTurmaActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String alunoSelecionado = (String) parent.getItemAtPosition(position);
+                Toast.makeText(CadTurmaActivity.this, "Clicado: "+position, Toast.LENGTH_SHORT).show();
                 if (!selectedAlunos.contains(alunoSelecionado)) {
                     selectedAlunos.add(alunoSelecionado);
                 }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        menosAnonimos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer menos = Integer.valueOf(alunosAnonimos.getText().toString());
+                if(menos > 0)
+                    menos --;
+                alunosAnonimos.setText(menos.toString());
+            }
+        });
+
+        maisAnonimos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer mais = Integer.valueOf(alunosAnonimos.getText().toString());
+                mais ++;
+                alunosAnonimos.setText(mais.toString());
             }
         });
         cadastrar.setOnClickListener(new View.OnClickListener() {

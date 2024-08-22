9b5b7dcc97a1e9487e6dbd1779d12fa8201e85ad
dev_felipe <felipemartinsdonascimento4@gmail.com>
2024-08-22 15:19:51 -0400

    Realizando alterações

--- app/src/main/java/online/padev/kariti/VisualEscolaActivity.java
+++ app/src/main/java/online/padev/kariti/VisualEscolaActivity.java
@@ -7,16 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class VisualEscolaActivity extends AppCompatActivity {
     ImageButton iconeSair;
-    ImageView btnEscolaDesativada, btnCadastrarEscola;
+    FloatingActionButton btnEscolaDesativada, btnCadastrarEscola;
     ImageButton iconeAjuda;
     TextView titulo;
     ListView listViewEscolas;

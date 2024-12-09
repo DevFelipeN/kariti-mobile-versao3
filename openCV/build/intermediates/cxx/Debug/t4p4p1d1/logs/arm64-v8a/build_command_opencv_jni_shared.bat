90b4d40e610a13f59cf108ca4b74b0a215757242
dev_felipe <felipemartinsdonascimento4@gmail.com>
2024-12-09 09:37:55 -0400

    OpenCV configurado ao projeto

---
+++ openCV/build/intermediates/cxx/Debug/t4p4p1d1/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Felipe\\sistema-kariti\\openCV\\.cxx\\Debug\\t4p4p1d1\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/Debug/t4p4p1d1/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\Felipe\\sistema-kariti\\openCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=23" ^
+  "-DANDROID_PLATFORM=android-23" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Felipe\\sistema-kariti\\openCV\\build\\intermediates\\cxx\\Debug\\t4p4p1d1\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Felipe\\sistema-kariti\\openCV\\build\\intermediates\\cxx\\Debug\\t4p4p1d1\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\Felipe\\sistema-kariti\\openCV\\.cxx\\Debug\\t4p4p1d1\\arm64-v8a" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"

54a710bb1662ef994acadf13523b4762104e5eb5
dev_felipe <felipemartinsdonascimento4@gmail.com>
2025-03-10 14:25:49 -0400

    Funcionalidade de tester implementado

---
+++ app/src/main/java/online/padev/kariti/emails/SendImageTester.java
@@ -0,0 +1,2 @@
+package online.padev.kariti.emails;public class SendImageTester {
+}
---
+++ app/src/main/java/online/padev/kariti/utils/CompactImage.java
@@ -0,0 +1,15 @@
+package online.padev.kariti.utils;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveImageTester {
+    public static List<String> controllerImage = new ArrayList<>();
+
+
+}

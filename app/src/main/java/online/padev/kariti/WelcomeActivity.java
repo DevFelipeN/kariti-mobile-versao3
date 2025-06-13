fda533a6e828f11451eee888da0023e1e952b2d6
dev_felipe <felipemartinsdonascimento4@gmail.com>
2025-06-13 16:06:53 -0400

    Icone para seleção de idioma adicionado

--- app/src/main/java/online/padev/kariti/WelcomeActivity.java
+++ app/src/main/java/online/padev/kariti/WelcomeActivity.java
@@ -1,20 +1,32 @@
 package online.padev.kariti;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.PopupMenu;
 import org.opencv.android.OpenCVLoader;
+
+import java.util.Locale;
 
 import online.padev.kariti.database.DataBaseKariti;
 
 public class WelcomeActivity extends AppCompatActivity {
     DataBaseKariti dataBase;
+    AppCompatImageView languageIcon;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ActivityLocale.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,7 @@ public class WelcomeActivity extends AppCompatActivity {
         AppCompatButton btnRegistration = findViewById(R.id.buttonCadastroW);
         AppCompatButton btnLogin = findViewById(R.id.buttonLoginW);
         AppCompatButton btnDefaultPassword = findViewById(R.id.buttonDefaultPassword);
+        languageIcon = findViewById(R.id.imageViewLanguage);
 
         if (OpenCVLoader.initDebug()) {
             Log.e("opencv","Sucesso na inicialização do openCV");
@@ -40,7 +53,35 @@ public class WelcomeActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(v -> startLogin());
         btnDefaultPassword.setOnClickListener(v -> startProvaDefault());
 
-        changeLanguage("pt");
+        languageIcon.setOnClickListener(v -> {
+            PopupMenu popup = new PopupMenu(this, v);
+            popup.getMenuInflater().inflate(R.menu.activity_menu_language, popup.getMenu());
+            Menu menu = popup.getMenu();
+            menu.setGroupCheckable(0, true, true);
+
+            SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+            String currentLang = prefs.getString("language", Locale.getDefault().getLanguage()); // padrão pt
+
+            if (currentLang.equals("pt")) {
+                menu.findItem(R.id.lang_pt).setChecked(true);
+            } else if (currentLang.equals("en")) {
+                menu.findItem(R.id.lang_en).setChecked(true);
+            }
+
+            popup.setOnMenuItemClickListener(item -> {
+                item.setChecked(true);
+                int id = item.getItemId();
+                if (id == R.id.lang_pt) {
+                    changeLanguage("pt");
+                    return true;
+                } else if (id == R.id.lang_en) {
+                    changeLanguage("en");
+                    return true;
+                }
+                return false;
+            });
+            popup.show();
+        });
     }
 
     /**
@@ -71,6 +112,7 @@ public class WelcomeActivity extends AppCompatActivity {
         prefs.edit().putString("app_language", languageCode).apply();
 
         ActivityLocale.setLocale(this, languageCode);
-        //recreate();  // reinicia a Activity para aplicar o idioma
+        recreate();  // reinicia a Activity para aplicar o idioma
     }
+
 }
--- openCV/build/intermediates/cxx/RelWithDebInfo/6f153b5z/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Felipe\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Felipe\\sistema-kariti\\openCV\\.cxx\\RelWithDebInfo\\6f153b5z\\x86" ^
-  opencv_jni_shared

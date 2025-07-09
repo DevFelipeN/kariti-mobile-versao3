f56edc4e3f9de71e573aa1cfaec07ed637c996b4
dev_felipe <felipemartinsdonascimento4@gmail.com>
2025-07-09 15:23:00 -0400

    StudentRegistration configurado para o ingles

--- app/src/main/java/online/padev/kariti/activitys/StudentRegistrationActivity.java
+++ app/src/main/java/online/padev/kariti/activitys/StudentRegistrationActivity.java
@@ -2,6 +2,8 @@ package online.padev.kariti.activitys;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
@@ -12,12 +14,18 @@ import android.widget.Toast;
 
 import online.padev.kariti.R;
 import online.padev.kariti.database.DataBaseKariti;
+import online.padev.kariti.settings.ActivityLocale;
 
 public class StudentRegistrationActivity extends AppCompatActivity {
     ImageButton back;
     EditText editTextNameStudent, EditTextEmail;
     Button btnSave;
     DataBaseKariti dataBase;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ActivityLocale.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,39 +45,39 @@ public class StudentRegistrationActivity extends AppCompatActivity {
                 String nameStudent = editTextNameStudent.getText().toString().trim();
                 String email = EditTextEmail.getText().toString().trim();
                 if (nameStudent.isEmpty()) {
-                    Toast.makeText(StudentRegistrationActivity.this, "Informe o nome do aluno", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(StudentRegistrationActivity.this, getString(R.string.toastInfoNameStudent), Toast.LENGTH_SHORT).show();
                     return;
                 }
                 Boolean checkStudent = dataBase.checkExistStudent(nameStudent);
                 if (checkStudent == null) {
-                    Toast.makeText(this, "Erro de comunicação! \n\n Por favor, tente novamente", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, getString(R.string.toastApplicationError), Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (checkStudent) {
-                    Toast.makeText(StudentRegistrationActivity.this, "Identificamos que esse aluno já está cadastrado!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, getString(R.string.toastStudentAlreadyRegister), Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (!email.isEmpty()) {
                     if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                        Toast.makeText(StudentRegistrationActivity.this, "E-mail do aluno, inválido!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(StudentRegistrationActivity.this, getString(R.string.toastInvalidEmail), Toast.LENGTH_SHORT).show();
                         return;
                     }
                     Boolean checkEmail = dataBase.checkStudentEmail(email);
                     if (checkEmail == null) {
-                        Toast.makeText(this, "Erro de comunicação! \n\n Por favor, tente novamente", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, getString(R.string.toastApplicationError), Toast.LENGTH_SHORT).show();
                         return;
                     }
                     if (checkEmail) {
-                        Toast.makeText(StudentRegistrationActivity.this, "Este e-mail já esta vinculado a um aluno cadastrado!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(StudentRegistrationActivity.this, getString(R.string.toastEmailAlreadyLinkStudent), Toast.LENGTH_SHORT).show();
                         return;
                     }
                 }
                 Integer insertStudent = dataBase.insertStudent(nameStudent, email, 1);
                 if (insertStudent != -1) {
-                    Toast.makeText(StudentRegistrationActivity.this, "Aluno cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(StudentRegistrationActivity.this, getString(R.string.toastStudentSuccessRegistration), Toast.LENGTH_SHORT).show();
                     restartVisualStudents();
                 } else {
-                    Toast.makeText(StudentRegistrationActivity.this, "Aluno não cadastrado!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(StudentRegistrationActivity.this, getString(R.string.toastStudentFailedRegistration), Toast.LENGTH_SHORT).show();
                 }
             }catch (Exception e){
                 Log.e("kariti", e.toString());
